66653e5bc7c8d6419815acdf898b8f01688177b2
ofcljaved <javedofficialme@gmail.com>
2025-04-07 18:14:27 +0530

    chore: main box scroll animaton

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,46 @@
-import { useMotionValueEvent, useScroll } from "motion/react"
+import { useScroll, motion, useMotionValueEvent } from "motion/react"
+import { useState } from "react";
 
 function App() {
-  const { scrollYProgress } = useScroll()
+  const { scrollY } = useScroll()
+  const [scrollDirection, setScrollDirection] = useState<string>("up");
 
-  useMotionValueEvent(scrollYProgress, "change", (latest) => {
-    console.log("Page scroll: ", latest)
+  useMotionValueEvent(scrollY, "change", (curr) => {
+    const diff = curr - scrollY.getPrevious()!;
+    if (diff > 0) setScrollDirection("down");
+    if (diff < 0 && curr <= 100) setScrollDirection("up");
   })
+
+  const variants = {
+    initial: {
+      '--dropbox-size': "calc(-2px + min(800px, -64px + min(100vw, 100vh)))",
+      '--dropbox-color': "#0061fe",
+      '--dropbox-bg': "#ffffff",
+    },
+    middle: {
+      '--dropbox-size': "calc(-2px + min(502px, -64px + min(100vw, 100vh)))",
+      '--dropbox-color': "#ffffff",
+      '--dropbox-bg': "#0061fe",
+    }
+  };
 
   return (
     <>
       <div
         className="flex justify-center items-center pointer-events-none fixed size-(--dropbox-btn-size) left-(--dropbox-left) bottom-(--dropbox-bottom) transform-(--dropbox-transform)"
       >
-        <div
-          className="border border-black size-(--dropbox-size) text-(--dropbox-color) flex-none"
+        <motion.div
+          initial={variants.initial}
+          variants={{
+            up: variants.initial,
+            down: variants.middle
+          }}
+          animate={scrollDirection}
+          transition={{ duration: 0.6, ease: [0.5, 0, 0.2, 1] }}
+          className="size-(--dropbox-size) text-(--dropbox-color) flex-none bg-(--dropbox-bg)"
         >
           <div>Dropbox</div>
-        </div>
+        </motion.div>
       </div>
     </>
   )
