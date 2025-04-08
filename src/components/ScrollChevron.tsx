38fd777014c470b58ab795b500cec5133c6290cf
ofcljaved <javedofficialme@gmail.com>
2025-04-09 03:25:46 +0530

    chore: add chevron animation

--- src/components/ScrollChevron.tsx
+++ src/components/ScrollChevron.tsx
@@ -1,9 +1,30 @@
 import { motion, SVGMotionProps } from "motion/react";
+import { useEffect, useState } from "react";
+import { useLoading } from "../context/loadingContext";
 
 export default function ScrollChevron() {
+  const [hasScrolled, setHasScrolled] = useState(false);
+
+  useEffect(() => {
+    const signal = new AbortController();
+    window.addEventListener("scroll", () => {
+      setHasScrolled(true);
+    }, { signal: signal.signal });
+    return () => signal.abort();
+  }, []);
+
   return (
     <motion.div
       className="absolute inset-0"
+      variants={{
+        hidden: { opacity: 0 },
+        visible: { opacity: 1, transition: { delay: 0.5, duration: 1 } }
+      }}
+      animate={hasScrolled ? "hidden" : "visible"}
+      transition={{
+        duration: 0.6,
+        ease: [0.5, 0, 0.2, 1],
+      }}
     >
       <Chevron
         className="bottom-[42px]"
@@ -30,12 +51,21 @@ export default function ScrollChevron() {
 interface ChevronProps extends SVGMotionProps<SVGSVGElement> { }
 
 const Chevron = ({ className, ...props }: ChevronProps) => {
+  const { loading } = useLoading();
+  const variants = {
+    stop: {
+      opacity: 0,
+      transform: "translatey(0px)"
+    },
+    start: {
+      opacity: [1, 0.75, 1],
+      transform: ["translatey(0px)", "translatey(18px)", "translatey(0px)"]
+    }
+  };
   return (
     <motion.svg
-      animate={{
-        opacity: [1, 0.75, 1],
-        transform: ["translatey(0px)", "translatey(18px)", "translatey(0px)"]
-      }}
+      variants={variants}
+      animate={loading ? "stop" : "start"}
       className={`w-[52px] right-[14px] absolute ${className}`} width="26" height="14" viewBox="0 0 26 14" fill="currentColor"
       {...props}
     >
