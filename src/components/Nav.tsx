10de19abf8998cd88cf8115ab614765db4e143d2
ofcljaved <javedofficialme@gmail.com>
2025-04-14 18:14:11 +0530

    chore: start grid lines

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,9 +1,11 @@
 import { NavContent } from "./NavContent";
+import { NavGrid } from "./NavGrid";
 
 export function Nav() {
   return (
     <div className="w-screen pointer-events-auto fixed inset-0 z-[100] overflow-hidden">
       <NavContent />
+      <NavGrid />
     </div>
   )
 }
---
+++ src/components/NavGrid.tsx
@@ -0,0 +1,23 @@
+import { Color } from "./Color";
+import { Iconography } from "./Iconography";
+import { Imagery } from "./Imagery";
+import { Logo } from "./Logo";
+import { Motion } from "./Motion";
+import { Strategy } from "./Strategy";
+import { Typography } from "./Typography";
+import { VoiceTone } from "./VoiceTone";
+
+export function NavGrid() {
+  return (
+    <div className="absolute inset-0 z-1 items-stretch *:absolute *:p-0 *:origin-[100%_100%] *:pointer-events-auto">
+      <Strategy index={0} />
+      <VoiceTone index={1} />
+      <Logo index={2} />
+      <Typography index={3} />
+      <Iconography index={4} />
+      <Color index={5} />
+      <Imagery index={6} />
+      <Motion index={7} />
+    </div>
+  )
+}
